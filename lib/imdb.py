91763e05e2c98ad79199c0af3eb3372cbdaee564
Ícaro Maicon <46580318+icarok99@users.noreply.github.com>
2025-07-16 21:29:47 -0300

    Update imdb.py

--- lib/imdb.py
+++ lib/imdb.py
@@ -9,41 +9,29 @@ except:
     from helper import *
 import re
 
-
-# def buscar_chaves(dicionario, chave_alvo, caminho_atual=""):
-#     if isinstance(dicionario, dict):
-#         for chave, valor in dicionario.items():
-#             novo_caminho = f"{caminho_atual}['{chave}']" if caminho_atual else f"['{chave}']"
-#             if chave == chave_alvo:
-#                 print(f"Chave encontrada: {novo_caminho} -> {valor}")
-#             buscar_chaves(valor, chave_alvo, novo_caminho)
-#     elif isinstance(dicionario, list):
-#         for index, item in enumerate(dicionario):
-#             novo_caminho = f"{caminho_atual}[{index}]"
-#             buscar_chaves(item, chave_alvo, novo_caminho)
-
 class IMDBScraper:
     def __init__(self):
         self.base = 'https://www.imdb.com'
         self.headers = {
-        'Accept-Language': 'pt-BR,pt;q=0.9,en;q=0.8'
+            'Accept-Language': 'pt-BR,pt;q=0.9,en;q=0.8'
         }
-    def soup(self,html):
+
+    def soup(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         return soup
 
-    def search_series(self,search):
+    def search_series(self, search):
         itens = []
         try:
             query = quote(search)
-            url = '{0}/find/?q={1}&s=tt&ttype=tv'.format(self.base,query)
-            html = cfscraper.get(url,headers=self.headers).text
-            json_ = re.findall(r'<script id="__NEXT_DATA__" type="application/json">(.+?)</script>', html,re.DOTALL)[0]
+            url = '{0}/find/?q={1}&s=tt&ttype=tv'.format(self.base, query)
+            html = cfscraper.get(url, headers=self.headers).text
+            json_ = re.findall(r'<script id="__NEXT_DATA__" type="application/json">(.+?)</script>', html, re.DOTALL)[0]
             data = json.loads(json_)
             results = data['props']['pageProps']['titleResults']['results']
             for idx, serie in enumerate(results):
                 imdb_id = serie['id']
-                page = self.base + '/title/' + imdb_id  +'/?ref_=fn_tt_tt_' + str(idx)
+                page = self.base + '/title/' + imdb_id + '/?ref_=fn_tt_tt_' + str(idx)
                 name = serie['titleNameText']
                 try:
                     year = serie['titleReleaseText']
@@ -53,31 +41,29 @@ class IMDBScraper:
                         pass
                 except:
                     year = '0'
-                try:
-                    img = serie['titlePosterImageModel']['url']
-                except:
-                    img = ''
-                # if year:
-                #     name = name + ' (' + str(year) + ')'
-                name = name.replace('&amp;', '&').replace('&apos;', "'")
-                itens.append((name,img,page,year,imdb_id))
-        except:
-            pass       
+                img = serie.get('titlePosterImageModel', {}).get('url', '')
+                if not img:
+                    continue  # Ignora conteúdos sem poster
+                name = name.replace('&', '&').replace('&apos;', "'")
+                itens.append((name, img, page, year, imdb_id))
+        except Exception as e:
+            print(f"[IMDB] Erro em search_series: {e}")
         return itens
 
-
-    def series_250(self):
+    def series_250(self, limit=250):
         itens = []
         try:
             url = self.base + '/chart/toptv/?ref_=nv_tvv_250'
-            html = cfscraper.get(url,headers=self.headers).text
-            json_ = re.findall(r'<script type="application/ld\+json">(.+?)</script>', html,re.DOTALL)[0]
+            html = cfscraper.get(url, headers=self.headers).text
+            json_ = re.findall(r'<script type="application/ld\+json">(.+?)</script>', html, re.DOTALL)[0]
             dict_ = json.loads(json_)
             series = dict_['itemListElement']
             if series:
+                count = 0
                 for i in series:
+                    if count >= limit:
+                        break
                     data = i['item']
-                    #name = data['name']
                     name = data.get('alternateName', data.get('name', ''))
                     tip = data['@type']
                     url = data['url']
@@ -85,30 +71,33 @@ class IMDBScraper:
                         description = data['description']
                     except:
                         description = ''
-                    image = data['image']
-                    #rate = data['aggregateRating']['ratingValue']
-                    #genre = data['genre']
+                    image = data.get('image', '')
+                    if not image:
+                        continue  # Ignora conteúdos sem poster
                     imdb_id = re.findall(r'/tt(.*?)/', url)[0]
                     imdb_id = 'tt' + imdb_id
-                    name = name.replace('&amp;', '&').replace('&apos;', "'")
-                    description = description.replace('&amp;', '&').replace('&apos;', "'")                    
-                    itens.append((name,image,url,description,imdb_id))
-        except:
-            pass
+                    name = name.replace('&', '&').replace('&apos;', "'")
+                    description = description.replace('&', '&').replace('&apos;', "'")
+                    itens.append((name, image, url, description, imdb_id))
+                    count += 1
+        except Exception as e:
+            print(f"[IMDB] Erro em series_250: {e}")
         return itens
-    
-    def series_popular(self):
+
+    def series_popular(self, limit=100):
         itens = []
         try:
             url = self.base + '/chart/tvmeter/?ref_=nv_tvv_mptv'
-            html = cfscraper.get(url,headers=self.headers).text
-            json_ = re.findall(r'<script type="application/ld\+json">(.+?)</script>', html,re.DOTALL)[0]
+            html = cfscraper.get(url, headers=self.headers).text
+            json_ = re.findall(r'<script type="application/ld\+json">(.+?)</script>', html, re.DOTALL)[0]
             dict_ = json.loads(json_)
             series = dict_['itemListElement']
             if series:
+                count = 0
                 for i in series:
+                    if count >= limit:
+                        break
                     data = i['item']
-                    #name = data['name']
                     name = data.get('alternateName', data.get('name', ''))
                     tip = data['@type']
                     url = data['url']
@@ -116,25 +105,24 @@ class IMDBScraper:
                         description = data['description']
                     except:
                         description = ''
-                    image = data['image']
-                    #rate = data['aggregateRating']['ratingValue']
-                    #genre = data['genre']
+                    image = data.get('image', '')
+                    if not image:
+                        continue  # Ignora conteúdos sem poster
                     imdb_id = re.findall(r'/tt(.*?)/', url)[0]
                     imdb_id = 'tt' + imdb_id
-                    name = name.replace('&amp;', '&').replace('&apos;', "'")
-                    description = description.replace('&amp;', '&').replace('&apos;', "'")
-                    itens.append((name,image,url,description,imdb_id))
-        except:
-            pass 
-        return itens      
+                    name = name.replace('&', '&').replace('&apos;', "'")
+                    description = description.replace('&', '&').replace('&apos;', "'")
+                    itens.append((name, image, url, description, imdb_id))
+                    count += 1
+        except Exception as e:
+            print(f"[IMDB] Erro em series_popular: {e}")
+        return itens
 
-
-    
-    def imdb_seasons(self,url):
+    def imdb_seasons(self, url):
         itens = []
         try:
-            html = cfscraper.get(url,headers=self.headers).text
-            json_ = re.findall(r'<script id="__NEXT_DATA__" type="application/json">(.+?)</script>', html,re.DOTALL)[0]
+            html = cfscraper.get(url, headers=self.headers).text
+            json_ = re.findall(r'<script id="__NEXT_DATA__" type="application/json">(.+?)</script>', html, re.DOTALL)[0]
             data = json.loads(json_)
             seasons = data['props']['pageProps']['mainColumnData']['episodes']['seasons']
             imdb_id = re.findall(r'/tt(.*?)/', url)[0]
@@ -144,15 +132,15 @@ class IMDBScraper:
                 name = '{0} temporada'.format(str(idx))
                 url_season = season_base_url + str(idx)
                 itens.append((str(season['number']), name, url_season))
-        except:
-            pass
+        except Exception as e:
+            print(f"[IMDB] Erro em imdb_seasons: {e}")
         return itens
 
-    def imdb_episodes(self,url):
+    def imdb_episodes(self, url):
         itens = []
         try:
-            html = cfscraper.get(url,headers=self.headers).text
-            json_ = re.findall(r'<script id="__NEXT_DATA__" type="application/json">(.+?)</script>', html,re.DOTALL)[0]
+            html = cfscraper.get(url, headers=self.headers).text
+            json_ = re.findall(r'<script id="__NEXT_DATA__" type="application/json">(.+?)</script>', html, re.DOTALL)[0]
             data = json.loads(json_)
             episodes = data['props']['pageProps']['contentData']['section']['episodes']['items']
             try:
@@ -163,30 +151,29 @@ class IMDBScraper:
                 for idx, episode in enumerate(episodes, start=1):
                     episode_number = str(idx)
                     name = episode.get('titleText', 'Episodio - ' + episode_number)
-                    try:
-                        img = episode['image']['url']
-                    except:
-                        img = ''
+                    img = episode.get('image', {}).get('url', '')
+                    if not img:
+                        continue  # Ignora conteúdos sem poster
                     description = episode.get('plot', '')
-                    name = name.replace('&amp;', '&').replace('&apos;', "'")
-                    description = description.replace('&amp;', '&').replace('&apos;', "'")
-                    itens.append((episode_number,name,img,fanart,description))
-        except:
-            pass
+                    name = name.replace('&', '&').replace('&apos;', "'")
+                    description = description.replace('&', '&').replace('&apos;', "'")
+                    itens.append((episode_number, name, img, fanart, description))
+        except Exception as e:
+            print(f"[IMDB] Erro em imdb_episodes: {e}")
         return itens
-    
-    def search_movies(self,search):
+
+    def search_movies(self, search):
         itens = []
         try:
             query = quote(search)
-            url = '{0}/find/?q={1}&s=tt&ttype=ft'.format(self.base,query)
-            html = cfscraper.get(url,headers=self.headers).text
-            json_ = re.findall(r'<script id="__NEXT_DATA__" type="application/json">(.+?)</script>', html,re.DOTALL)[0]
+            url = '{0}/find/?q={1}&s=tt&ttype=ft'.format(self.base, query)
+            html = cfscraper.get(url, headers=self.headers).text
+            json_ = re.findall(r'<script id="__NEXT_DATA__" type="application/json">(.+?)</script>', html, re.DOTALL)[0]
             data = json.loads(json_)
             results = data['props']['pageProps']['titleResults']['results']
             for idx, movie in enumerate(results):
                 imdb_id = movie['id']
-                page = self.base + '/title/' + imdb_id  +'/?ref_=fn_tt_tt_' + str(idx)
+                page = self.base + '/title/' + imdb_id + '/?ref_=fn_tt_tt_' + str(idx)
                 name = movie['titleNameText']
                 try:
                     year = movie['titleReleaseText']
@@ -196,30 +183,29 @@ class IMDBScraper:
                         pass
                 except:
                     year = '0'
-                try:
-                    img = movie['titlePosterImageModel']['url']
-                except:
-                    img = ''
-                # if year:
-                #     name = name + ' (' + str(year) + ')'
-                name = name.replace('&amp;', '&').replace('&apos;', "'")
-                itens.append((name,img,page,year,imdb_id))
-        except:
-            pass       
-        return itens    
+                img = movie.get('titlePosterImageModel', {}).get('url', '')
+                if not img:
+                    continue  # Ignora conteúdos sem poster
+                name = name.replace('&', '&').replace('&apos;', "'")
+                itens.append((name, img, page, year, imdb_id))
+        except Exception as e:
+            print(f"[IMDB] Erro em search_movies: {e}")
+        return itens
 
-    def movies_250(self):
+    def movies_250(self, limit=250):
         itens = []
         try:
             url = self.base + '/chart/top/?ref_=nv_mv_250'
-            html = cfscraper.get(url,headers=self.headers).text
-            json_ = re.findall(r'<script type="application/ld\+json">(.+?)</script>', html,re.DOTALL)[0]
+            html = cfscraper.get(url, headers=self.headers).text
+            json_ = re.findall(r'<script type="application/ld\+json">(.+?)</script>', html, re.DOTALL)[0]
             dict_ = json.loads(json_)
             movies = dict_['itemListElement']
             if movies:
+                count = 0
                 for i in movies:
+                    if count >= limit:
+                        break
                     data = i['item']
-                    #name = data['name']
                     name = data.get('alternateName', data.get('name', ''))
                     tip = data['@type']
                     url = data['url']
@@ -227,30 +213,33 @@ class IMDBScraper:
                         description = data['description']
                     except:
                         description = ''
-                    image = data['image']
-                    #rate = data['aggregateRating']['ratingValue']
-                    #genre = data['genre']
+                    image = data.get('image', '')
+                    if not image:
+                        continue  # Ignora conteúdos sem poster
                     imdb_id = re.findall(r'/tt(.*?)/', url)[0]
                     imdb_id = 'tt' + imdb_id
-                    name = name.replace('&amp;', '&').replace('&apos;', "'")
-                    description = description.replace('&amp;', '&').replace('&apos;', "'") 
-                    itens.append((name,image,url,description,imdb_id))
-        except:
-            pass
+                    name = name.replace('&', '&').replace('&apos;', "'")
+                    description = description.replace('&', '&').replace('&apos;', "'")
+                    itens.append((name, image, url, description, imdb_id))
+                    count += 1
+        except Exception as e:
+            print(f"[IMDB] Erro em movies_250: {e}")
         return itens
 
-    def movies_popular(self):
+    def movies_popular(self, limit=100):
         itens = []
         try:
             url = self.base + '/chart/moviemeter/?ref_=nv_mv_mpm'
-            html = cfscraper.get(url,headers=self.headers).text
-            json_ = re.findall(r'<script type="application/ld\+json">(.+?)</script>', html,re.DOTALL)[0]
+            html = cfscraper.get(url, headers=self.headers).text
+            json_ = re.findall(r'<script type="application/ld\+json">(.+?)</script>', html, re.DOTALL)[0]
             dict_ = json.loads(json_)
             movies = dict_['itemListElement']
             if movies:
+                count = 0
                 for i in movies:
+                    if count >= limit:
+                        break
                     data = i['item']
-                    #name = data['name']
                     name = data.get('alternateName', data.get('name', ''))
                     tip = data['@type']
                     url = data['url']
@@ -258,14 +247,15 @@ class IMDBScraper:
                         description = data['description']
                     except:
                         description = ''
-                    image = data['image']
-                    #rate = data['aggregateRating']['ratingValue']
-                    #genre = data['genre']
+                    image = data.get('image', '')
+                    if not image:
+                        continue  # Ignora conteúdos sem poster
                     imdb_id = re.findall(r'/tt(.*?)/', url)[0]
                     imdb_id = 'tt' + imdb_id
-                    name = name.replace('&amp;', '&').replace('&apos;', "'")
-                    description = description.replace('&amp;', '&').replace('&apos;', "'")
-                    itens.append((name,image,url,description,imdb_id)) 
-        except:
-            pass       
-        return itens 
+                    name = name.replace('&', '&').replace('&apos;', "'")
+                    description = description.replace('&', '&').replace('&apos;', "'")
+                    itens.append((name, image, url, description, imdb_id))
+                    count += 1
+        except Exception as e:
+            print(f"[IMDB] Erro em movies_popular: {e}")
+        return itens

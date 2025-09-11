96680769f05cb1af8799ce6632e7a5923f32bfb0
Ícaro Maicon <46580318+icarok99@users.noreply.github.com>
2025-09-11 12:21:43 -0300

    Update api_vod.py

--- lib/api_vod.py
+++ lib/api_vod.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-try:
-    from lib.ClientScraper import cfscraper, USER_AGENT
-except ImportError:
-    from ClientScraper import cfscraper, USER_AGENT
+# try:
+#     from lib.ClientScraper import cfscraper, USER_AGENT
+# except ImportError:
+#     from ClientScraper import cfscraper, USER_AGENT
 try:
     from lib.helper import *
 except:
     from helper import *
 import re
+import requests
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0'}
 
@@ -38,7 +39,7 @@ class VOD:
                 r_ = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_url_r)
                 headers_ = headers
                 headers_.update({'sec-fetch-dest': 'iframe'})                
-                r = cfscraper.get(url, headers=headers_)
+                r = requests.get(url, headers=headers_)
                 src = r.text
                 soup = BeautifulSoup(src, 'html.parser')
                 try:
@@ -54,20 +55,20 @@ class VOD:
                 api = '{0}/api'.format(self.base)
                 headers_.pop('sec-fetch-dest', None)
                 headers_.update({'origin': self.base, 'referer': url})
-                r = cfscraper.post(api,data={'action': 'getOptions', 'contentid': data_contentid}, headers=headers_)
+                r = requests.post(api,data={'action': 'getOptions', 'contentid': data_contentid}, headers=headers_)
                 src = r.json()
                 id_ = src['data']['options'][0]['ID']
-                r = cfscraper.post(api,data={'action': 'getPlayer', 'video_id': id_}, headers=headers_)
+                r = requests.post(api,data={'action': 'getPlayer', 'video_id': id_}, headers=headers_)
                 src = r.json()
                 video_url = src['data']['video_url']
                 video_hash = video_url.split('/')[-1]
                 parsed_url = urlparse(video_url)
                 origin = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_url)
                 player = '{uri.scheme}://{uri.netloc}/player/index.php?data={0}&do=getVideo'.format(video_hash, uri=parsed_url)
-                r = cfscraper.get(video_url, headers={'User-Agent': headers_['User-Agent'], 'sec-fetch-dest': 'iframe'})
+                r = requests.get(video_url, headers={'User-Agent': headers_['User-Agent'], 'sec-fetch-dest': 'iframe'})
                 cookies_dict = r.cookies.get_dict()
                 cookie_string = urlencode(cookies_dict)
-                r = cfscraper.post(player,headers={'Origin': origin, 'x-requested-with': 'XMLHttpRequest'}, data={'hash': str(video_hash), 'r': r_}, cookies=cookies_dict)
+                r = requests.post(player,headers={'Origin': origin, 'x-requested-with': 'XMLHttpRequest'}, data={'hash': str(video_hash), 'r': r_}, cookies=cookies_dict)
                 src = r.json()
                 stream = src['videoSource'] + '|User-Agent=' + quote_plus(headers_['User-Agent']) + '&Cookie=' + quote_plus(cookie_string)
         except:
@@ -83,7 +84,7 @@ class VOD:
                 r_ = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_url_r)
                 headers_ = headers
                 headers_.update({'sec-fetch-dest': 'iframe'})
-                r = cfscraper.get(url, headers=headers_)
+                r = requests.get(url, headers=headers_)
                 src = r.text
                 soup = BeautifulSoup(src, 'html.parser')
                 div = soup.find('div',{'class': 'players_select'}) # dublado
@@ -91,20 +92,25 @@ class VOD:
                 api = '{0}/api'.format(self.base)
                 headers_.pop('sec-fetch-dest', None)
                 headers_.update({'origin': self.base, 'referer': url})
-                r = cfscraper.post(api,data={'action': 'getPlayer', 'video_id': data_id}, headers=headers_)
+                r = requests.post(api,data={'action': 'getPlayer', 'video_id': data_id}, headers=headers_)
                 src = r.json()
                 video_url = src['data']['video_url']
                 video_hash = video_url.split('/')[-1]
                 parsed_url = urlparse(video_url)
                 origin = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_url)
                 player = '{uri.scheme}://{uri.netloc}/player/index.php?data={0}&do=getVideo'.format(video_hash, uri=parsed_url)
-                r = cfscraper.get(video_url, headers={'User-Agent': headers_['User-Agent'], 'sec-fetch-dest': 'iframe'})
+                r = requests.get(video_url, headers={'User-Agent': headers_['User-Agent'], 'sec-fetch-dest': 'iframe'})
                 cookies_dict = r.cookies.get_dict()
                 cookie_string = urlencode(cookies_dict)
-                r = cfscraper.post(player,headers={'Origin': origin, 'x-requested-with': 'XMLHttpRequest'}, data={'hash': str(video_hash), 'r': r_}, cookies=cookies_dict)
+                r = requests.post(player,headers={'Origin': origin, 'x-requested-with': 'XMLHttpRequest'}, data={'hash': str(video_hash), 'r': r_}, cookies=cookies_dict)
                 src = r.json()
                 stream = src['videoSource'] + '|User-Agent=' + quote_plus(headers_['User-Agent']) + '&Cookie=' + quote_plus(cookie_string)
         except:
             pass
         return stream
+
+#print(VOD().movie('tt0106697'))
+#print(VOD().tvshows('tt3107288','1','1'))
+    
+
 

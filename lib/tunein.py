b65dd92a3726e94f0b40e8eed4f172b743ec8a3f
Ícaro Maicon <46580318+icarok99@users.noreply.github.com>
2026-02-18 06:45:19 -0300

    Add files via upload

--- lib/tunein.py
+++ lib/tunein.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
 from lib.helper import *
-try:
-    from lib.ClientScraper import cfscraper
-except ImportError:
-    from ClientScraper import cfscraper
+import requests
 
 
 def radios_list(API):
     try:
-        radios = cfscraper.get(API).json()
+        radios = requests.get(API, allow_redirects=True).json()
         setcontent('music')
         fanart = 'https://conteudo.imguol.com.br/c/entretenimento/4f/2017/11/24/mulher-ouvindo-musica-1511535013801_v2_4x3.jpg'
         for i in radios:
-            name = i['name']
-            iconimage = i['logo']
-            stream = i['stream']
+            name = i.get('name', '')
+            iconimage = i.get('logo', '')
+            stream = i.get('stream', '')
             li = xbmcgui.ListItem(name)
             li.setProperty('IsPlayable', 'true')
             li.setArt({"icon": "DefaultVideo.png", "thumb": iconimage})
@@ -25,6 +22,6 @@ def radios_list(API):
                 li.setInfo('music', {'mediatype': 'song'})
             li.setProperty('fanart_image', fanart)
             xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=stream, listitem=li, isFolder=False)
-        end()            
+        end()
     except:
         pass
